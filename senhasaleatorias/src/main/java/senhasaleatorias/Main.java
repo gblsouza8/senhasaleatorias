8b78ef5c514e1307dc94a4ba93beaeff44690356
Gabriel de Souza <gabrielsouza080802@gmail.com>
2025-03-02 09:20:00 -0300

    Segundo commit

--- senhasaleatorias/src/main/java/senhasaleatorias/Main.java
+++ senhasaleatorias/src/main/java/senhasaleatorias/Main.java
@@ -1,7 +1,29 @@
 package senhasaleatorias;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
+    @SuppressWarnings("ConvertToTryWithResources")
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        int qtd;
+
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Insira a quantidade de caracteres na senha: ");
+        qtd = in.nextInt();
+        in.close();
+
+
+        String caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%¨&()_-+=";
+        Random aleatorio = new Random();
+        StringBuilder senha = new StringBuilder();
+
+        for (int i = 0; i < qtd; i++) {
+            int indice = aleatorio.nextInt(caracteres.length());
+            senha.append(caracteres.charAt(indice));
+        }
+
+        System.out.println("Sua senha com " + qtd + " caracteres aleatórios é: " + senha.toString());
+
+        }
 }
